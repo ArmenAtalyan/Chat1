9dd0326d8f4edc576a40b8773f546122fdf82e63
ArmenAtalyan <atalyan3@gmail.com>
2019-12-22 23:31:50 +0400

    divide function of methodes

--- src/ChatMember.java
+++ src/ChatMember.java
@@ -28,32 +28,35 @@ public class ChatMember {
             System.out.println("\n" + "Press number for action.");
             Scanner sc3 = new Scanner(System.in);
             int forAct = sc3.nextInt();
-
-            if(forAct > 2 || forAct < 1){
-                System.out.println("Invalid input.");
-                Thread.sleep(1000);
-                System.out.println("\n" + "Press number for action.");
-                Scanner sc4 = new Scanner(System.in);
-                forAct = sc4.nextInt();
-                index -= 1;
-            }
-
-            switch(forAct){
-                case(1):
-                    Thread.sleep(1000);
-                    System.out.println("Send message.");
-                    Scanner scanner3 = new Scanner(System.in);
-                    String messageText = scanner3.nextLine();
-
-                        User user = new User(index, data, memberNumList[index], messageText);
-                        System.out.println(user);
-                    break;
-                case(2):
-                    Thread.sleep(1000);
-                    System.out.println("Exit chat.");
-                    memberNumList[index] = null;
-                    memberSum --;
-            }
+            memberSum = workWithMemberCall(forAct, index, data, memberSum, memberNumList);
         }
+    }
+
+
+    public int workWithMemberCall(int forAct, int index, String data, int memberSum, String[] memberNumList) throws InterruptedException{
+        if(forAct > 2 || forAct < 1){
+            System.out.println("Invalid input.");
+            Thread.sleep(1000);
+            System.out.println("\n" + "Press again number for action.");
+            Scanner sc4 = new Scanner(System.in);
+            forAct = sc4.nextInt();
+            index -= 1;
+        }
+        switch(forAct){
+            case(1):
+                Thread.sleep(1000);
+                System.out.println("Send message.");
+                Scanner scanner3 = new Scanner(System.in);
+                String messageText = scanner3.nextLine();
+                User user = new User(index, data, memberNumList[index], messageText);
+                System.out.println(user);
+                break;
+            case(2):
+                Thread.sleep(1000);
+                System.out.println("Exit chat.");
+                memberNumList[index] = null;
+                memberSum --;
+        }
+        return memberSum;
     }
 }
