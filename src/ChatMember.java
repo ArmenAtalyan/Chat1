0913bc2f0014786d086b0a225f90c8b2cda85e68
ArmenAtalyan <atalyan3@gmail.com>
2019-12-23 23:01:42 +0400

    try to solve incorrect inpute problem

--- src/ChatMember.java
+++ src/ChatMember.java
@@ -8,6 +8,8 @@ public class ChatMember {
     public void memberCall()throws InterruptedException{
         Format f = new SimpleDateFormat("mm-dd-yyyy hh:mm:ss");
         String data = f.format(new Date());
+
+        System.out.println("Enter number of members.");
         Scanner sc1 = new Scanner(System.in);
         int memberSum = sc1.nextInt();
         String[] memberNameList = new String[memberSum];
@@ -15,26 +17,26 @@ public class ChatMember {
         while(memberSum > 0){
            int index = 1 + (int)(Math.random() * memberSum - 1);
            if(memberNameList[index] == null) {
-               Thread.sleep(1000);
                 System.out.println("\n" + "Enter your name.");
                 Scanner sc2 = new Scanner(System.in);
-                String memberNume = sc2.nextLine();
-                memberNameList[index] = memberNume;
+                String memberName = sc2.nextLine();
+                memberNameList[index] = memberName;
             }
-           else{
+             else{
                Thread.sleep(1000);
                System.out.println("\n" + memberNameList[index] + " is typing again.");
            }
-            Thread.sleep(1000);
+
             System.out.println("\n" + "Press number for action.");
             Scanner sc3 = new Scanner(System.in);
             int forAct = sc3.nextInt();
-            memberSum = workWithMemberCall(forAct, index, data, memberSum, memberNameList);
+            memberSum = workWithInputNumber(forAct, index, data, memberSum, memberNameList);
+
         }
     }
 
 
-    public int workWithMemberCall(int forAct, int index, String data, int memberSum, String[] memberNameList) throws InterruptedException{
+    public int workWithInputNumber(int forAct, int index, String data, int memberSum, String[] memberNameList) throws InterruptedException{
 
         if(forAct < 1 || forAct > 2){
             System.out.println("Invalid input.");
@@ -42,11 +44,9 @@ public class ChatMember {
             System.out.println("\n" + "Press again number for action.");
             Scanner sc4 = new Scanner(System.in);
             forAct = sc4.nextInt();
-            index -= 1;
         }
         switch(forAct){
             case(1):
-                Thread.sleep(1000);
                 System.out.println("Send message.");
                 Scanner scanner3 = new Scanner(System.in);
                 String messageText = scanner3.nextLine();
@@ -54,8 +54,8 @@ public class ChatMember {
                 System.out.println(user);
                 break;
             case(2):
-                Thread.sleep(1000);
                 System.out.println("Exit chat.");
+                Thread.sleep(1000);
                 memberNameList[index] = null;
                 memberSum --;
         }
