da412d73e4ec1adf00c5576992c419c1f0accab2
ArmenAtalyan <atalyan3@gmail.com>
2019-12-25 22:48:11 +0400

    delete thread sleeps

--- src/ChatMember.java
+++ src/ChatMember.java
@@ -4,7 +4,7 @@ public class ChatMember {
 
     User user = new User();
 
-    public void memberCall() throws InterruptedException {
+    public void memberCall() {
 
         System.out.println("Enter number of members.");
         Scanner sc1 = new Scanner(System.in);
@@ -12,23 +12,22 @@ public class ChatMember {
         String[] memberNameList = new String[memberSum];
 
         while (memberSum > 0) {
-            int index = 1 + (int) (Math.random() * memberSum - 1);
+            int index = 1+(int)(Math.random()*memberSum - 1);
             if (memberNameList[index] == null) {
-                System.out.println("\n" + "Enter your name.");
+                System.out.println("Enter your name.");
                 Scanner sc2 = new Scanner(System.in);
                 String memberName = sc2.nextLine();
                 memberNameList[index] = memberName;
             } else {
-                Thread.sleep(1000);
-                System.out.println("\n" + memberNameList[index] + " is typing again.");
+                System.out.println("\n" + memberNameList[index] + " is typing.");
             }
-            System.out.println("1. Send message" + "\n" + "2. Exit chat" + "\n" + "Press number for action.");
+            System.out.println("Press number for action.");
             memberSum = workWithInputNumber(sc1.next(), index, user.getDate(), memberSum, memberNameList);
         }
     }
 
 
-    public int workWithInputNumber(String forAct, int index, String date, int memberSum, String[] memberNameList) throws InterruptedException {
+    public int workWithInputNumber(String forAct, int index, String date, int memberSum, String[] memberNameList) {
 
         Scanner sc4 = new Scanner(System.in);
         while (!Character.isDigit(forAct.charAt(0)) || Integer.parseInt(forAct) < 1 || Integer.parseInt(forAct) > 2) {
@@ -39,14 +38,11 @@ public class ChatMember {
         switch (forAct) {
             case ("1"):
                 System.out.println("Send message.");
-                Scanner scanner3 = new Scanner(System.in);
-                String messageText = scanner3.nextLine();
-                User user = new User(index, date, memberNameList[index], messageText);
+                User user = new User(index, date, memberNameList[index], sc4.nextLine());
                 System.out.println(user);
                 break;
             case ("2"):
                 System.out.println( memberNameList[index] + " Exit chat.");
-                Thread.sleep(1000);
                 memberNameList[index] = null;
                 memberSum--;
         }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) {
         ChatMember member = new ChatMember();
         member.memberCall();
     }
