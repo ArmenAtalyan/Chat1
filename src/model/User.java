1c0d089a6c059321d3bbba69cd1fcaa34783ef30
ArmenAtalyan <atalyan3@gmail.com>
2020-01-18 01:16:20 +0400

    some changes in user

--- src/model/User.java
+++ src/model/User.java
@@ -9,24 +9,14 @@ public class User {
         private int id;
         private String name;
         private String text;
-        private String date;
         Format f = new SimpleDateFormat("mm-dd-yyyy hh:mm:ss");
 
         public User(){}
 
     public User(int id, String name, String text) {
         this.id = id;
-        this.date = f.format(new Date());
         this.name = name;
         this.text = text;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
     }
 
     public String getName() {
